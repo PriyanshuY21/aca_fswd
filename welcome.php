94473719354900862dd53c59eb2ddef04b08f8f8
Priyanshu <106882732+PriyanshuY21@users.noreply.github.com>
2023-12-12 20:58:00 +0530

    123test

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+// Check if the user is not logged in
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("location: login.php");
+    exit;
+}
+
+// Define the HTML page link
+$htmlPageLink = "task_nitrosk.html";
+
+// Redirect the user to the HTML page
+header("location: $htmlPageLink");
+exit;
+?>
